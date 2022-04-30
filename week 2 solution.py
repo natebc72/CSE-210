99823758e190f17f6bfe1e67af0607c00efa0ce7
natebc72 <nbchristiansen72@gmail.com>
2022-04-30 14:37:42 -0700

    Week 2 with solution

--- week 2 solution.py
+++ week 2 solution.py
@@ -1,25 +1,31 @@
+'''
+Tic-Tac-Toe: A Solution
+Author: Bro. Manley
+'''
+
 def main():
     player = next_player("")
-    board = make_board()
+    board = create_board()
     while not (has_winner(board) or is_a_draw(board)):
         display_board(board)
         make_move(player, board)
         player = next_player(player)
     display_board(board)
-    print("Great Game! T'was a pleasure!") 
+    print("Good game. Thanks for playing!") 
 
-def make_board():
+def create_board():
     board = []
     for square in range(9):
         board.append(square + 1)
     return board
 
 def display_board(board):
-    print(f"{board[0]}|{board[3]}|{board[6]}")
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
     print('-+-+-')
-    print(f"{board[1]}|{board[4]}|{board[7]}")
+    print(f"{board[3]}|{board[4]}|{board[5]}")
     print('-+-+-')
-    print(f"{board[2]}|{board[5]}|{board[8]}")
+    print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
     
 def is_a_draw(board):
@@ -39,7 +45,7 @@ def has_winner(board):
             board[2] == board[4] == board[6])
 
 def make_move(player, board):
-    square = int(input(f"{player}'s turn (Pick a # 1-9): "))
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
     board[square - 1] = player
 
 def next_player(current):
